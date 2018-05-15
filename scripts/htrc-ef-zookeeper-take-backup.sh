7dceb6d104821070981513994c531961b9dc3c91
davidb <davidb@immensity.cms.waikato.ac.nz>
2018-05-15 15:37:35 +1200

    Tweak after testing

--- scripts/htrc-ef-zookeeper-take-backup.sh
+++ scripts/htrc-ef-zookeeper-take-backup.sh
@@ -1,1 +1,11 @@
-./zoocreeper dump -z $ZOOKEEPER_SERVER
+#!/bin/bash
+
+backup_file=${1:-zoocreeper-backup.json}
+
+echo ""
+echo "Saving backup to: $backup_file"
+echo ""
+
+zoocreeper dump -z $ZOOKEEPER_SERVER > $backup_file
+
+
