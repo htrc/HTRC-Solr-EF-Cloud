82d3b3bd8b8770032221139b27ecd85ea06fda21
David Bainbridge <dbbridge@lis-host.lis.illinois.edu>
2018-05-30 18:24:59 -0500

    Improved formatting of into

--- scripts/htrc-ef-solr-local-list-configsets.sh
+++ scripts/htrc-ef-solr-local-list-configsets.sh
@@ -2,10 +2,17 @@
 
 echo ""
 echo "Configsets in $SOLR_HOME:"
-ls "$SOLR_HOME/configsets/"
+solr_configsets=`ls "$SOLR_HOME/configsets/"`
+for sc in $solr_configsets ; do
+    echo "  $sc"
+done
 
 echo ""
 echo "Configsets/Collections in Zookeeper $ZOOKEEPER_SERVER"
-htrc-ef-zookeeper-cli.sh ls /configs | htrc-ef-zookeeper-cli-sanitize.awk
+zookeeper_configs=`htrc-ef-zookeeper-cli.sh ls /configs | htrc-ef-zookeeper-cli-sanitize.awk`
+
+echo $zookeeper_configs | egrep '^\[' \
+    | sed 's/^\[/ /' | sed 's/\]$//' | tr ',' '\n' \
+    | sed 's/^ /  /' | sort
 echo ""
 
