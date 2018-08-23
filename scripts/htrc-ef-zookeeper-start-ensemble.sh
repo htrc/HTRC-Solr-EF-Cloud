1d47d83db8cd946f81da55c400cb8627eec77bec
David Bainbridge <dbbridge@lis-host.lis.illinois.edu>
2018-08-22 20:16:32 -0500

    variable name fix

--- scripts/htrc-ef-zookeeper-start-ensemble.sh
+++ scripts/htrc-ef-zookeeper-start-ensemble.sh
@@ -3,6 +3,6 @@
 zookeeper_host=${ZOOKEEPER_SERVER%:*}
 
 for zookeeper_host in solr3 solr4 solr5 ; do
-    echo "Starting Zookeeper Ensemble node on $h"
+    echo "Starting Zookeeper Ensemble node on $zookeeper_host"
     ssh $zookeeper_host zkServer.sh start
 done
