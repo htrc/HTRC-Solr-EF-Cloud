bec3bf868af5c4be577259b83b06b9f2ae2d3521
David Bainbridge <davidb@waikato.ac.nz>
2020-04-11 06:31:18 -0500

    Additional useful script for working with zookeeper

---
+++ scripts/htrc-ef-zookeeper-status.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+zookeeper_host=${ZOOKEEPER_SERVER%:*}
+
+# Zookeeper config file (which sets port etc) is determined by Zookeeper home
+# Zookeeper home is auto-magically determined from filesystem location of where zkServer.sh
+
+ssh $zookeeper_host "$ZOOKEEPER_HOME/bin/zkServer.sh" status
+
