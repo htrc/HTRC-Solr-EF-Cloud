91819537cd57b69eb3ddb9aa5f3ef63611a4a763
David Bainbridge <davidb@waikato.ac.nz>
2020-04-12 06:14:50 -0500

    Change in where the solr index shares are located on disk

--- scripts/htrc-ef-solr-check-local-shardsize-all.sh
+++ scripts/htrc-ef-solr-check-local-shardsize-all.sh
@@ -6,7 +6,13 @@ echo "Running 'du -hs' on all Solr shards"
 
 local_dir="/hdfsd05/dbbridge/solr-ef"
 
+#for solr_host in $unique_hosts ; do
+##  ssh $solr_host du -hs /tmp/solr-ef/solr/*shard*;
+#  ssh $solr_host du -hs $local_dir/*shard*;
+#done
+
+solr_col=${1:-faceted-htrc-full-ef20}
+
 for solr_host in $unique_hosts ; do
-#  ssh $solr_host du -hs /tmp/solr-ef/solr/*shard*;
-  ssh $solr_host du -hs $local_dir/*shard*;
+  ssh $solr_host du -hs "/disk*/solr-full-ef/${solr_col}_shard*_replica*";
 done
