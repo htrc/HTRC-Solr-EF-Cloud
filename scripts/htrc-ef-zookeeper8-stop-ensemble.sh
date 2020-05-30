9a71bafcf3fa25c745d54984ca69ac4b46b6ae3e
David Bainbridge <davidb@waikato.ac.nz>
2020-05-29 23:53:32 -0500

    Changed to take advantage of htrc-ef-zookeeper8-cli-ensemble.sh

--- scripts/htrc-ef-zookeeper8-stop-ensemble.sh
+++ scripts/htrc-ef-zookeeper8-stop-ensemble.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-for zookeeper_host in solr3 solr4 solr5 ; do
-    echo "Stopping Zookeeper Ensemble node on $zookeeper_host"
-
-    # Zookeeper config file (which sets port etc) is determined by Zookeeper home
-    # Zookeeper home is auto-magically determined from filesystem location of where zkServer.sh
-
-    ssh $zookeeper_host "$ZOOKEEPER8_HOME/bin/zkServer.sh" stop
-done
+htrc-ef-zookeeper8-cli-ensemble.sh stop
