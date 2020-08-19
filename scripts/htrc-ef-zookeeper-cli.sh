c4df5c73f7f14d5ff451c5fd5430737f4fa66b79
David Bainbridge <davidb@waikato.ac.nz>
2020-08-18 23:00:43 -0500

    Script made more general so it can work with either a standalone Zookeeper server or an Ensemble

--- scripts/htrc-ef-zookeeper-cli.sh
+++ scripts/htrc-ef-zookeeper-cli.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-"$ZOOKEEPER_HOME/bin/zkCli.sh" -server $ZOOKEEPER_SERVER $*
+if [ "x$ZOOKEEPER_SERVER_ENSEMBLE" != "x" ] ; then
+    zookeeper_server=${ZOOKEEPER_SERVER_ENSEMBLE%%,*}
+else
+    zookeeper_server=$ZOOKEEPER_SERVER
+fi
+
+"$ZOOKEEPER_HOME/bin/zkCli.sh" -server $zookeeper_server $*
--- scripts/htrc-ef-zookeeper8-cli.sh
+++ scripts/htrc-ef-zookeeper8-cli.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-"$ZOOKEEPER8_HOME/bin/zkCli.sh" -server $ZOOKEEPER8_SERVER $*
+if [ "x$ZOOKEEPER8_SERVER_ENSEMBLE" != "x" ] ; then
+    zookeeper8_server=${ZOOKEEPER8_SERVER_ENSEMBLE%%,*}
+else
+    zookeeper8_server=$ZOOKEEPER8_SERVER
+fi
+
+"$ZOOKEEPER8_HOME/bin/zkCli.sh" -server $zookeeper8_server $*
