e88a006c6548f822f47d84154f3ba0bde9d1d5cb
dbbridge <dbbridge@lis-host.ischool.illinois.edu>
2021-03-08 04:52:30 -0600

    New sed clause added to deal with hosthames such as htrc-solr7 that we want to map to solr7

--- scripts/htrc-ef-short-hostname.sh
+++ scripts/htrc-ef-short-hostname.sh
@@ -3,5 +3,6 @@
 # To make short (local) hostname:
 #   i) remove full domain trailing suffix
 #   ii) remove 'is-' prefix that turns up on machines like solr3
+#   iii) remove 'htrc-' prefix that turns up on machines like solr7
 
-echo $(uname -n | sed 's/\..*//' | sed 's/^is-//' | sed 's/peachpalm/solr7/' | sed 's/royalpalm/solr8/')
+echo $(uname -n | sed 's/\..*//' | sed 's/^is-//' | sed 's/^htrc-//' | sed 's/peachpalm/solr7/' | sed 's/royalpalm/solr8/')
